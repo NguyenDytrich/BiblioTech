98c18e2896966a330c3ff99a7182b6601b2b6115
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2020-09-25 13:12:39 -0700

    Scaffold login view

--- equilizer/views.py
+++ equilizer/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
@@ -20,3 +21,15 @@ class ItemGroupDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+def login_view(request):
+    username = request.POST["username"***REMOVED***
+    password = request.POST["password"***REMOVED***
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Nav to success page
+    else:
+        # Return invalid error msg
+        pass

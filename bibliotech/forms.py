8629ea36890e77b89f45d7de44a26c84ee1e468d
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2020-10-15 09:51:36 -0700

    Add tests for form validation for udpate password

--- bibliotech/forms.py
+++ bibliotech/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
@@ -31,3 +32,29 @@ class SignupForm(forms.Form):
                     "password_confirm",
                     ValidationError(_("Passwords don't match!")),
                 )
+
+
+class UpdateProfileForm(forms.Form):
+    fname = forms.CharField()
+    lname = forms.CharField()
+    email = forms.EmailField()
+
+
+class UpdatePasswordForm(forms.Form):
+    user_id = forms.IntegerField()
+    password = forms.CharField()
+    new_password = forms.CharField()
+    new_confirmed = forms.CharField()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user = User.objects.get(pk=cleaned_data.get("user_id"))
+        current_pwd = cleaned_data.get("password")
+        new_pwd = cleaned_data.get("new_password")
+        new_conf = cleaned_data.get("new_confirmed")
+        if user and current_pwd:
+            if not user.check_password(current_pwd):
+                self.add_error("password", ValidationError(_("Incorrect password")))
+        if new_pwd and new_conf:
+            if new_pwd != new_conf:
+                self.add_error("new_confirmed", ValidationError(_("Passwords don't match!")))
--- bibliotech/tests/test_user_profile.py
+++ bibliotech/tests/test_user_profile.py
@@ -5,6 +5,7 @@ from parameterized import parameterized
 
 from library.models import Member
 
+
 class UserProfileTests(TransactionTestCase):
     """
     Users should be able to see all their profile information
@@ -29,8 +30,6 @@ class UserProfileTests(TransactionTestCase):
         aux_member = Member.objects.create(user=aux, member_id="123457")
         aux_member.save()
 
-
-
     def test_information_in_view(self):
         """
         User information should be visible to the user in the rendered html
@@ -43,11 +42,12 @@ class UserProfileTests(TransactionTestCase):
         self.assertContains(response, self.user.email)
         self.assertContains(response, self.member.member_id)
 
-
-    @parameterized.expand([
-        ("member", "password"),
-        ("member2", "password"),
-        ])
+    @parameterized.expand(
+        [
+            ("member", "password"),
+            ("member2", "password"),
+        ]
+    )
     def test_profile_context(self, username, password):
         """
         The profile context should contain the user's information
@@ -59,17 +59,24 @@ class UserProfileTests(TransactionTestCase):
         self.assertTrue(profile)
         self.assertEqual(profile.user.username, username)
 
-
     def test_profile_redirects_anon_user(self):
         """
         If an anon user makes a request to the user profile, should redirect to login
         """
         response = self.client.get(reverse("user-profile"))
-        self.assertRedirects(response, f"{reverse('login')}?next={reverse('user-profile')}")
+        self.assertRedirects(
+            response, f"{reverse('login')}?next={reverse('user-profile')}"
+        )
 
-    @parameterized.expand([
-        ("Valid (Don Juan)", {"email": "new@email.com", "fname": "Don", "lname": "Juan"}, 200)
-        ])
+    @parameterized.expand(
+        [
+            (
+                "Valid (Don Juan)",
+                {"email": "new@email.com", "fname": "Don", "lname": "Juan"},
+                200,
+            )
+        ]
+    )
     def test_update_profile(self, name, fields, expected):
         """
         Post request to the update-profile should update a specified field
@@ -88,12 +95,40 @@ class UserProfileTests(TransactionTestCase):
         if fields.get("lname"):
             self.assertEQual(user.last_name, fields["lname"])
 
-    @parameterized.expand([
-            ("Valid password change", {"old_password": "password", "new_password": "password2", "new_confirmed": "password2"}, 200)
-        ])
+    @parameterized.expand(
+        [
+            (
+                "Valid password change",
+                {
+                    "user_id": 1,
+                    "password": "password",
+                    "new_password": "password2",
+                    "new_confirmed": "password2",
+                },
+                200,
+            )
+        ]
+    )
     def test_change_password(self, name, fields, expected_status):
         self.client.login(username="member", password=fields["old_password"])
         response = self.client.post(reverse("user-change-password"))
 
         user = User.objects.get(username="member")
         self.assertTrue(user.check_password(fields["new_password"]))
+
+    def test_change_password_wrong_user(self):
+        """
+        A user should not be able to make a request to change another user's password
+        """
+        self.client.login(username="member2", password="password")
+
+        fields = {
+            "user_id": 1,
+            "password": "password",
+            "new_password": "password2",
+            "new_confirmed": "password2",
+        }
+
+        response = self.client.post(reverse("user-profile-update"), fields)
+
+        self.assertEqual(response.status_code, 403)

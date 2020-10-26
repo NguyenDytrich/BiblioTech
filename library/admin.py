31ac09276aff6d6deb565bea812e0e43f937f703
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2020-10-26 10:28:04 -0700

    Add tag panel to master inventory view, optimize queries for master inventory view

--- library/admin.py
+++ library/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from library.models import Checkout, Item, ItemGroup, Member
+from library.models import Checkout, Item, ItemGroup, Member, Tag
 
 # Register your models here.
 admin.site.register(Checkout)
 admin.site.register(ItemGroup)
 admin.site.register(Item)
 admin.site.register(Member)
+admin.site.register(Tag)
